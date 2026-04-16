c3d3098cc598bef3bd2749e1edb6bf2c852d50d4
chibani <info@loicg.net>
2026-04-16 20:23:43 +0200

    fix dictionnaries

--- test/unit/woody.test.js
+++ test/unit/woody.test.js
@@ -1,27 +1,35 @@
 import { loadProfile } from '../../src/profile-loader.js';
 import woodyProfile from '../../data/profiles/woody.json';
+import titavionProfile from '../../data/profiles/titavion.json';
 
-const config = loadProfile(woodyProfile);
+const profiles = [woodyProfile, titavionProfile];
 
-test('Should use the special template on April first', () => {
-    jest.useFakeTimers().setSystemTime(new Date('2023-04-01'));
-    expect(config.generate_greetings((word) => word)).toBe('Salut les algues, les anguilles et les poissons');
-});
+describe('Woody profile', () => {
+    const config = loadProfile(woodyProfile);
 
-test('Should use the default template any day but april first', () => {
-    jest.useFakeTimers().setSystemTime(new Date('2023-01-01'));
-    expect(config.generate_greetings((word) => word)).toBe('Salut les copeaux, les échardes et les sciures');
-});
-
-test('Should use the special template on 2025 birthday stream', () => {
-    jest.useFakeTimers().setSystemTime(new Date('2025-03-15'));
-    expect(config.generate_greetings((word) => word)).toBe('Joyeux copeauversaire les copeaux, les échardes et les sciures');
-});
-
-test('Dictionary should not contain duplicates in adjectives', () => {
-    const adjectives = config.adjectives;
-    let duplicated = adjectives.filter((element, index) => {
-        return adjectives.indexOf(element) !== index;
+    test('Should use the special template on April first', () => {
+        jest.useFakeTimers().setSystemTime(new Date('2023-04-01'));
+        expect(config.generate_greetings((word) => word)).toBe('Salut les algues, les anguilles et les poissons');
     });
-    expect(duplicated).toStrictEqual([]);
+
+    test('Should use the default template any day but april first', () => {
+        jest.useFakeTimers().setSystemTime(new Date('2023-01-01'));
+        expect(config.generate_greetings((word) => word)).toBe('Salut les copeaux, les échardes et les sciures');
+    });
+
+    test('Should use the special template on 2025 birthday stream', () => {
+        jest.useFakeTimers().setSystemTime(new Date('2025-03-15'));
+        expect(config.generate_greetings((word) => word)).toBe('Joyeux copeauversaire les copeaux, les échardes et les sciures');
+    });
+});
+
+describe('All profiles', () => {
+    test.each(profiles)('Dictionary should not contain duplicates in adjectives for profile $id', (profile) => {
+        const config = loadProfile(profile);
+        const adjectives = config.adjectives;
+        const duplicated = adjectives.filter((element, index) => {
+            return adjectives.indexOf(element) !== index;
+        });
+        expect(duplicated).toStrictEqual([]);
+    });
 });
